7f57737af955f16e86bd0a107cc18320dfdb1b9a
Elvin <example>
2021-12-26 17:18:07 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+const str = "teSt";
+// const arr = [1, 2, 3, 4, 6];
+
+console.log(str.toLowerCase());
+console.log(str); 
+
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("q"));
+    
+const logg = "Hello World";
+
+console.log(logg.slice(6));
